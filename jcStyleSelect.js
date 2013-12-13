1172538ce6993df1be014ee60de16f6952cbefca
John Chappell <john.chappell@bottlerocketapps.com>
2013-12-13 15:00:02 -0600

    fixes issue with two selects being able to exist in an app at one time and show different values

--- jcStyleSelect.js
+++ jcStyleSelect.js
@@ -16,24 +16,24 @@ angular.module('jcStyleSelect', [])
                     pre: function preLink(scope, element, attrs) {
                         var el = element[0];
                         var ngClass = attrs.ngClass || '';
-                        var e, lastValue, selectElement, template;
+                        var e, lastValue, selectElement, selectValueElement, template;
 
                         el.removeAttribute('jc-style-select');
                         el.removeAttribute('class');
                         el.removeAttribute('ng-class');
 
                         template = '<span ng-class="' + ngClass + '" class="jc-style-select" >' +
-                            '<span class="jc-style-select-value" >{{ selectValue }}</span>' +
+                            '<span class="jc-style-select-value" ></span>' +
                             '<span class="jc-style-select-control-wrapper">' + el.outerHTML + '</span>' +
                             '</span>';
 
                         e = $compile(template)(scope);
                         selectElement = e.find('select')[0];
+                        selectValueElement = e.find('span')[0];
                         element.replaceWith(e);
 
                         scope.$watch(function () {
                             var thisValue;
-
                             if (selectElement.selectedIndex == -1) {
                                 return;
                             }
@@ -41,7 +41,8 @@ angular.module('jcStyleSelect', [])
                             thisValue = selectElement.options[selectElement.selectedIndex].text;
 
                             if (thisValue !== lastValue) {
-                                scope.selectValue = thisValue;
+
+                                selectValueElement.innerText = thisValue;
                             }
 
                             lastValue = thisValue;
